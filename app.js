76a1d607a4bbfea58c707d08b6a61356b9af504c
nickode <nicolasnsamaha@hotmail.com>
2018-08-29 14:47:32 -0400

    Added main function for making calls to Alpha Venture API. StockData array is made null upon exiting getStocks() which is a bug

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 var express = require("express");
 var request = require("request");
-var app = express();
 
-var key = "6FEH5E1H0ZR89A8V";
+var app = express();
 var port = 3000;
 
-app.get("/url", function(req, res, next){
-    request(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSE:WEED&apikey={key}`, function(){
+var stocks = ["TSE:WEED","TSE:ACB"];
 
-    });
+function getStocks(Stocks, res){
+
+    var key = "6FEH5E1H0ZR89A8V";
+    var StockData = [];
+
+    var jsonContent = null;
+
+    var stock_symbol = null;
+    var stock_last = null;
+    var stock_current_price = null;
+
+    for(var i = 0; i < Stocks.length; i++){
+        request(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${Stocks[i]}&apikey=${key}`, function(err, res, body){
+            jsonContent = JSON.parse(body);
+
+            //store data inside Javascript object
+            stock_symbol = jsonContent['Meta Data']['2. Symbol'];
+            stock_last = jsonContent['Meta Data']['3. Last Refreshed'];
+            stock_current_price = jsonContent['Time Series (Daily)'][stock_last]['4. close'];
+
+            var stockObject = {symbol:stock_symbol, last_updated:stock_last, current_price:stock_current_price};
+            StockData.push(stockObject);
+
+            console.log('Stock Symbol: ' + stockObject.symbol + ', Last Updated: ' + stockObject.last_updated + ', Current Price: ' + stockObject.current_price + '$');
+        });
+    }
+
+}
+
+app.get('/', function(req,res){
+    getStocks(stocks, res);
 });
 
 app.listen(port, function(){
-    console.log("Server running on PORT: " + port);
-} );
+
+});
+
+
 
