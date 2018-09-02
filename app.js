276a9c18f6d24b17ee9013c8f05c31248af2d536
nickode <nicolasnsamaha@hotmail.com>
2018-09-02 05:44:31 -0400

    Added fs functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var request = require("request");
+var fs = require('fs');
+
 
 var app = express();
 var port = 3000;
@@ -11,6 +13,7 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
 
+    fs.open('stocks.txt', 'w', function(err, fd){ if (err) throw err;});
     var key = "6FEH5E1H0ZR89A8V";
     var StockData = [];
 
@@ -19,15 +22,24 @@ app.get('/', function(req,res){
         request(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stocks[i]}&apikey=${key}`, function(err, response, body){
             var jsonContent = JSON.parse(body);
 
-            StockData.push({
+            var stockObject = {
                 symbol:jsonContent['Meta Data']['2. Symbol'],
                 current_price:jsonContent['Time Series (Daily)'][jsonContent['Meta Data']['3. Last Refreshed']]['4. close'],
                 last_updated:jsonContent['Meta Data']['3. Last Refreshed']
+            };
+
+            fs.open('stocks.txt', 'a', function(err, fd){
+                fs.appendFile(fd, `${stockObject.symbol},${stockObject.current_price},${stockObject.last_updated}\n`, 'utf8', function(err){
+                    fs.close(fd, function(err){
+                        if (err) throw err;
+                    });
+                });
             });
-            
         });
 
     }
+    
+    
 
     res.render('index',{StockData: StockData});
 });
