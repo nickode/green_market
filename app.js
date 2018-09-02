817a540857f81668cd9b946afeed6bb303698503
nickode <nicolasnsamaha@hotmail.com>
2018-09-02 04:27:29 -0400

    Added index ejs view and cleaned up some messy code. Still working on handling api requests from Alpha Vantage

--- app.js
+++ app.js
@@ -6,41 +6,34 @@ var port = 3000;
 
 var stocks = ["TSE:WEED","TSE:ACB"];
 
-function getStocks(Stocks, res){
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
 
     var key = "6FEH5E1H0ZR89A8V";
     var StockData = [];
 
-    var jsonContent = null;
+    for(var i = 0; i < stocks.length; i++){
 
-    var stock_symbol = null;
-    var stock_last = null;
-    var stock_current_price = null;
+        request(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stocks[i]}&apikey=${key}`, function(err, response, body){
+            var jsonContent = JSON.parse(body);
 
-    for(var i = 0; i < Stocks.length; i++){
-        request(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${Stocks[i]}&apikey=${key}`, function(err, res, body){
-            jsonContent = JSON.parse(body);
-
-            //store data inside Javascript object
-            stock_symbol = jsonContent['Meta Data']['2. Symbol'];
-            stock_last = jsonContent['Meta Data']['3. Last Refreshed'];
-            stock_current_price = jsonContent['Time Series (Daily)'][stock_last]['4. close'];
-
-            var stockObject = {symbol:stock_symbol, last_updated:stock_last, current_price:stock_current_price};
-            StockData.push(stockObject);
-
-            console.log('Stock Symbol: ' + stockObject.symbol + ', Last Updated: ' + stockObject.last_updated + ', Current Price: ' + stockObject.current_price + '$');
+            StockData.push({
+                symbol:jsonContent['Meta Data']['2. Symbol'],
+                current_price:jsonContent['Time Series (Daily)'][jsonContent['Meta Data']['3. Last Refreshed']]['4. close'],
+                last_updated:jsonContent['Meta Data']['3. Last Refreshed']
+            });
+            
         });
+
     }
 
-}
-
-app.get('/', function(req,res){
-    getStocks(stocks, res);
+    res.render('index',{StockData: StockData});
 });
 
 app.listen(port, function(){
-
+    console.log("Server listening on port " + port);
 });
 
 
