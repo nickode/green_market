9f4627f3efb3d86804a55251778d219891e3c194
nickode <nicolasnsamaha@hotmail.com>
2018-09-07 19:27:03 -0400

    Changed app to be run on command line only. Removed expressjs node module from development

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-var express = require("express");
 var request = require("request");
-var fs = require('fs');
 
+var stocks = ["TSE:WEED","TSE:ACB","TSE:APH"];
+var key = "6FEH5E1H0ZR89A8V";
 
-var app = express();
-var port = 3000;
-
-var stocks = ["TSE:WEED","TSE:ACB"];
-
-
-app.set('view engine', 'ejs');
-
-app.get('/', function(req,res){
-
-    fs.open('stocks.txt', 'w', function(err, fd){ if (err) throw err;});
-    var key = "6FEH5E1H0ZR89A8V";
-    var StockData = [];
-
+function GetStocks(){
     for(var i = 0; i < stocks.length; i++){
-
         request(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stocks[i]}&apikey=${key}`, function(err, response, body){
             var jsonContent = JSON.parse(body);
 
@@ -28,25 +14,12 @@ app.get('/', function(req,res){
                 last_updated:jsonContent['Meta Data']['3. Last Refreshed']
             };
 
-            fs.open('stocks.txt', 'a', function(err, fd){
-                fs.appendFile(fd, `${stockObject.symbol},${stockObject.current_price},${stockObject.last_updated}\n`, 'utf8', function(err){
-                    fs.close(fd, function(err){
-                        if (err) throw err;
-                    });
-                });
-            });
+            console.log('Symbol:' + stockObject.symbol + ', Current Price:' + stockObject.current_price + '$, Last Updated:' + stockObject.last_updated);
         });
-
     }
-    
-    
+}
 
-    res.render('index',{StockData: StockData});
-});
-
-app.listen(port, function(){
-    console.log("Server listening on port " + port);
-});
+GetStocks();
 
 
 
